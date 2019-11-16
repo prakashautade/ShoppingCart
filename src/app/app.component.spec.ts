cfca2ec1907643c3df37c77cff34357b87156a38
Prakash Autade <prakash.autade@gmail.com>
2019-11-16 15:32:14 +0530

    Fixed unit test ngbCollapse

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [ AppComponent, NavigationBarComponent ],
+      imports: [ BrowserModule, NgbModule ]
     }).compileComponents();
   }));
 
--- src/app/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/navigation-bar/navigation-bar.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationBarComponent } from './navigation-bar.component';
+import { AppComponent } from '../app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
@@ -8,9 +11,9 @@ describe('NavigationBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationBarComponent ]
-    })
-    .compileComponents();
+      declarations: [ AppComponent, NavigationBarComponent ],
+      imports: [ BrowserModule, NgbModule ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
